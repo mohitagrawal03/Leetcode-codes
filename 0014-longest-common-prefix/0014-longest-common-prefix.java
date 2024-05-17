a7a47d5d58e7d13a8590785b5834bbd0cf0f0777
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-05-17 22:36:53 +0530

    Time: 1 ms (75.57%), Space: 41 MB (90.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,21 +1,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
-        
-        // Find the string with the minimum length
         int minIndex = 0;
         for (int i = 1; i < strs.length; i++) {
             if (strs[i].length() < strs[minIndex].length()) {
                 minIndex = i;
             }
         }
-        
         String minString = strs[minIndex];
-        
-        // Use a StringBuilder for efficient string concatenation
         StringBuilder commonPrefix = new StringBuilder();
-        
-        // Compare each character of the shortest string with all other strings
         for (int i = 0; i < minString.length(); i++) {
             boolean same = true;
             for (int j = 0; j < strs.length; j++) {
@@ -30,7 +23,6 @@ class Solution {
                 break;
             }
         }
-        
         return commonPrefix.toString();
     }
 }
