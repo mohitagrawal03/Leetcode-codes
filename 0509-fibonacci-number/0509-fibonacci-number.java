9e77f0081369593d0061ed67bc202bdf8036f0a3
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-05-22 15:39:00 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (55.85%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,12 @@
 class Solution {
     public int fib(int n) {
+        Integer dp[] = new Integer[n+1];
+        return fibHelper(n, dp);
+    }
+    public int fibHelper(int n, Integer[] dp) {
+        if(dp[n] != null) return dp[n];
         if(n == 0 || n == 1) return n;
-        
-        return fib(n-1) + fib(n-2);
+        dp[n] = fibHelper(n-1, dp) + fibHelper(n-2, dp);
+        return dp[n];
     }
 }
