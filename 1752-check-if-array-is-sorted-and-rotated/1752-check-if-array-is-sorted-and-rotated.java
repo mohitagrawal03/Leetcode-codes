7293f1c21a29dfe544c8af8cbe0d0f052098ae51
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-05 23:15:22 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (54.46%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,24 +1,37 @@
 class Solution {
-    public boolean check(int[] nums) {
-        int[] rot_nums = new int[nums.length];
-        for(int x = 0; x < nums.length; x++){
-            boolean sorted = true;
+//     public boolean check(int[] nums) {
+//         int[] rot_nums = new int[nums.length];
+//         for(int x = 0; x < nums.length; x++){
+//             boolean sorted = true;
             
-            for(int i = 0; i < nums.length; i++){
-                rot_nums[(i+x) % nums.length] = nums[i];
+//             for(int i = 0; i < nums.length; i++){
+//                 rot_nums[(i+x) % nums.length] = nums[i];
+//             }
+            
+//             for(int i = 0; i < rot_nums.length-1; i++){
+//                 if(rot_nums[i+1] < rot_nums[i]){
+//                     sorted = false;
+//                     break;
+//                 }
+//             }
+            
+//             if(sorted){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+    
+    public boolean check(int[] A) {
+        int k = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            if (A[i] > A[(i + 1) % n]) {
+                k++;
             }
-            
-            for(int i = 0; i < rot_nums.length-1; i++){
-                if(rot_nums[i+1] < rot_nums[i]){
-                    sorted = false;
-                    break;
-                }
-            }
-            
-            if(sorted){
-                return true;
+            if (k > 1) {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
