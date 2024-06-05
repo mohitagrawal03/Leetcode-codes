46da92c108f573f9c9544d5d41c2c8694550a0b6
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-05 22:39:24 +0530

    Time: 2 ms (99.05%), Space: 42.7 MB (78.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,41 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         s = s.toLowerCase();
+        
+//         StringBuilder ns = new StringBuilder();
+//         for (int i = 0; i < s.length(); i++) {
+//             if (Character.isLetterOrDigit(s.charAt(i))) {
+//                 ns.append(s.charAt(i));
+//             }
+//         }
+        
+//         for (int i = 0; i < ns.length() / 2; i++) {
+//             if (ns.charAt(i) != ns.charAt(ns.length() - 1 - i)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
         
         while (left < right) {
-            // Move left pointer to the right while it's not pointing to a letter or digit
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
-            // Move right pointer to the left while it's not pointing to a letter or digit
+            
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
-            // Compare characters at left and right pointers
+            
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            // Move both pointers towards the center
+            
             left++;
             right--;
         }
