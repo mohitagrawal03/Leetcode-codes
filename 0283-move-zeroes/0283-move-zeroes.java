c74284ad4472172dc90b8425181b4876837a54f6
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-10 00:26:40 +0530

    Time: 2 ms (83.98%), Space: 46.3 MB (16.05%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,48 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int temp;
-        int count = 0;
+        // very high time complexity but works
+        // int temp;
+        // int count = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] == 0){
+        //         count++;
+        //     }
+        // }
+        // for(int i = 0; i < count; i++){
+        //     for(int j = 1; j < nums.length-i; j++){
+        //         if(nums[j-1] == 0 && nums[j] != nums[j-1]){
+        //             temp = nums[j];
+        //             nums[j] = nums[j-1];
+        //             nums[j-1] = temp;
+        //         }
+        //     }
+        // }
+        
+        // int i = 0;
+        // int j = 0;
+        // int temp;
+        // while(){
+        //     if(nums[i] != 0){
+        //         i++;
+        //         j++;
+        //     }else if(nums[j] == 0){
+        //         j++;
+        //     }else {
+        //         temp = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //     }
+        // }
+        
+        // SNOWBALL METHOD
+        int snowballSize = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 0){
-                count++;
-            }
-        }
-        for(int i = 0; i < count; i++){
-            for(int j = 1; j < nums.length-i; j++){
-                if(nums[j-1] == 0 && nums[j] != nums[j-1]){
-                    temp = nums[j];
-                    nums[j] = nums[j-1];
-                    nums[j-1] = temp;
-                }
+                snowballSize++;
+            } else if(snowballSize > 0){
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i - snowballSize] = temp;
             }
         }
     }
