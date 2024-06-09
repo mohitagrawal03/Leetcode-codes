7b04cd8d9fd1c685cb84e80f7a564caf4b2a2518
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-09 22:55:22 +0530

    Time: 1 ms (76.66%), Space: 45 MB (5.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,29 +1,31 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int[] ans = new int[nums.length];
+//         int k = 0;
+//         ans[0] = nums[0];
+//         for(int i = 1; i < nums.length; i++){
+//             if(ans[k] < nums[i]){
+//                 k++;
+//                 ans[k] = nums[i];
+//             }
+//         }
+//         for(int i = 0; i < nums.length; i++){
+//             nums[i] = ans[i];
+//         }
+//         return k;
+//     }
+// }
+
 class Solution {
-    // public int removeDuplicates(int[] nums) {
-    //     int[] res = new int[nums.length];
-    //     int k = 0;
-    //     int count = 1;
-    //     res[0] = nums[0];
-    //     for(int i = 1; i < nums.length; i++){
-    //         if(res[k] != nums[i]){
-    //             k++;
-    //             res[k] = nums[i];
-    //             count++;
-    //         }
-    //     }
-    //     for(int i = 0; i < nums.length; i++){
-    //         nums[i] = res[i];
-    //     }
-    //     return count;
-    // }
     public int removeDuplicates(int[] nums) {
-        int k = 0;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[k] != nums[i]){
+        if (nums.length == 0) return 0;
+        int k = 0; // Points to the index of the last unique element
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k]) {
                 k++;
                 nums[k] = nums[i];
             }
         }
-        return k+1;
+        return k + 1; // k is zero-based, so we need to add 1 to get the count
     }
 }
