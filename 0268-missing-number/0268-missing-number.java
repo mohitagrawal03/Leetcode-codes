cd3b24de8235fbf0687c5b76aa296e185604b226
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-10 00:43:25 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (23.34%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int s = (n*(n+1))/2;
+        int sDash = 0;
+        for(int i = 0; i < nums.length; i++){
+            sDash += nums[i];
+        }
+        
+        return s - sDash;
+    }
+}
