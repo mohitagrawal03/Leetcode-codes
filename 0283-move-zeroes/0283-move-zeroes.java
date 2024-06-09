60606494ebb4b91d293b693917ae9d0462bd153d
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-09 23:51:50 +0530

    Time: 176 ms (5.03%), Space: 45.6 MB (85.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -8,7 +8,7 @@ class Solution {
             }
         }
         for(int i = 0; i < count; i++){
-            for(int j = 1; j < nums.length; j++){
+            for(int j = 1; j < nums.length-i; j++){
                 if(nums[j-1] == 0 && nums[j] != nums[j-1]){
                     temp = nums[j];
                     nums[j] = nums[j-1];
