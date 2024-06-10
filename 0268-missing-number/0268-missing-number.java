ad4df87aae33144e372dc5cdc10be42a9ba9b03d
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-10 20:52:30 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (92.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        //int n = nums.length;
-        int s = (nums.length*(nums.length+1))/2; // sum of n natural nos.
-        int sDash = 0;
-        for(int i = 0; i < nums.length; i++){
-            sDash += nums[i];
+        
+        // This one method and nothing wrong in it with right time and space comp.
+//         //int n = nums.length;
+//         int s = (nums.length*(nums.length+1))/2; // sum of n natural nos.
+//         int sDash = 0;
+//         for(int i = 0; i < nums.length; i++){
+//             sDash += nums[i];
+//         }
+        
+//         return s - sDash;
+        
+        // Approach Two
+        int xor = 0, i = 0;
+        for(i = 0; i < nums.length; i++){
+            xor ^= i ^ nums[i];
         }
         
-        return s - sDash;
+        return xor ^ i;
     }
 }
