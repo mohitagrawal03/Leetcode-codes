0660cb1cbbfa9b9195a091e1e28c075afa9cf3c3
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-11 21:49:06 +0530

    Time: 2 ms (97.73%), Space: 44.6 MB (67.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,47 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        
+        // Time complexity is high but works
+        // int[] result = new int[2];
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(nums[i]+nums[j] == target){
+        //             result[0] = i;
+        //             result[1] = j;
+        //             return result;
+        //         }
+        //     }
+        // }
+        // return result;
+        
+        // Incomplete solution
+        // Arrays.sort(nums);
+        // int[] result = new int[2];
+        // int i = 0, j = nums.length - 1;
+        // while(i != j){
+        //     if(nums[i] + nums[j] > target){
+        //         j--;
+        //     } else if(nums[i] + nums[j] < target){
+        //         i++;
+        //     } else{
+        //         result[0] = i;
+        //         result[1] = j;
+        //         break;
+        //     }
+        // }
+        // return result;
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        int compliment;
         for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+            compliment = target - nums[i];
+            if(hm.containsKey(compliment)){
+                return new int[]{hm.get(compliment), i};
+            } else{
+                hm.put(nums[i], i);
             }
         }
-        return result;
+        
+        return new int[]{};
     }
 }
