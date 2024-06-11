559e975fde5706207267818f52b67598a8921927
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-11 22:11:42 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (27.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int i, countZ = 0, countO = 0;
+        for(i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                countZ++;
+            } else if(nums[i] == 1){
+                countO++;
+            }
+        }
+        i = 0;
+        while(i < nums.length){
+            if(i < countZ){
+                nums[i] = 0;
+            } else if(i < countO + countZ){
+                nums[i] = 1;
+            } else{
+                nums[i] = 2;
+            }
+            i++;
+        }
     }
 }
