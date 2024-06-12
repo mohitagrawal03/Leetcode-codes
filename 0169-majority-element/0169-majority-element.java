d07b0ac42d7925c90271e11ea2ce62a65b2a5d23
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-12 21:13:36 +0530

    Time: 1 ms (99.81%), Space: 52.6 MB (64.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
+        // Using HashMap, linear T.C. but there is a better approach 
+//         int n = nums.length;
+//         Map<Integer, Integer> map = new HashMap<>();
         
-        n = n / 2;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > n) {
-                return entry.getKey();
+//         for (int i = 0; i < n; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+        
+//         n = n / 2;
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() > n) {
+//                 return entry.getKey();
+//             }
+//         }
+        
+//         return 0;
+        
+        // Moore Voting Algo
+        int count = 0, cand = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                cand = nums[i];
+                count++;
+            } else if (nums[i] == cand){
+                count++;
+            } else{
+                count--;
             }
         }
-        
-        return 0;
+        return cand;
     }
 }
