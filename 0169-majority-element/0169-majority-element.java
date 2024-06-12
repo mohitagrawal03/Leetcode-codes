edf5b47e538c60e96f5079f242a3e96c5e4ce9ed
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-12 07:27:07 +0530

    Time: 17 ms (14.16%), Space: 49 MB (92.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> freq = new HashMap<>();
-        
+        int max = nums.length%2 == 0 ? nums.length/2 : nums.length/2 + 1;
         for(int i = 0; i < nums.length; i++){
             if(freq.containsKey(nums[i])){
                 freq.put(nums[i], freq.get(nums[i])+1);
             } else{
                 freq.put(nums[i], 1);
             }
-        }
-        int maxKey = -1;
-        int maxValue = Integer.MIN_VALUE;
-
-        // Iterate through the map
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (entry.getValue() > maxValue) {
-                maxValue = entry.getValue();
-                maxKey = entry.getKey();
+            if(freq.get(nums[i]) >= max){
+                    return nums[i];
             }
         }
-        return maxKey;
+//         int maxKey = -1;
+//         int maxValue = Integer.MIN_VALUE;
+
+//         // Iterate through the map
+//         for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+//             if (entry.getValue() > maxValue) {
+//                 maxValue = entry.getValue();
+//                 maxKey = entry.getKey();
+//             }
+//         }
+        return 0;
     }
 }
