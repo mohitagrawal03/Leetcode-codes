720bf12fbb41195dca8d12c7a2cab9242a9a08bb
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-25 17:59:46 +0530

    Time: 4 ms (70.53%), Space: 42.2 MB (43.85%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,23 +1,45 @@
 class Solution {
-    public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
+    
+    // Time complexity O(n) and works and good
+//     public String removeOuterParentheses(String s) {
+//         Stack<Character> st = new Stack<>();
+//         StringBuilder ans = new StringBuilder();
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') {
+//                 if (!st.empty()) {
+//                     ans.append("(");
+//                 }
+                
+//                 st.push('(');
+//             } else {
+//                 st.pop();
+                
+//                 if (!st.empty()) {
+//                     ans.append(')');
+//                 }
+//             }
+//         }
+        
+//         return ans.toString();
+//     }
+    
+    // Little bit better Approach
+    public String removeOuterParentheses(String S) {
+        
         StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                if (!st.empty()) {
-                    ans.append("(");
-                }
-                
-                st.push('(');
+        int opened = 0, start = 0;
+        
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                opened++;
             } else {
-                st.pop();
-                
-                if (!st.empty()) {
-                    ans.append(')');
-                }
+                opened--;
+            }
+            if (opened == 0) {
+                ans.append(S.substring(start+1, i));
+                start = i+1;
             }
         }
-        
         return ans.toString();
     }
 }
