8f53f7325981417381edbc1e583ce0a7de05efa3
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-25 22:39:44 +0530

    Time: 7 ms (69.57%), Space: 43 MB (66.92%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,30 +1,11 @@
-// class Solution {
-//     public String reverseWords(String s) {
-//         String[] arr = s.trim().split("\\s+");
-//         StringBuilder sb = new StringBuilder();
-//         for (int i = arr.length-1; i > 0; i--) {
-//             sb.append(arr[i]).append(" ");
-//         }
-//         sb.append(arr[0]);
-//         return sb.toString();
-//     }
-// }
-
 class Solution {
     public String reverseWords(String s) {
-        // Trim the input string to remove leading and trailing spaces
-        String[] str = s.trim().split("\\s+");
-
-        // Initialize the output string
-        String out = "";
-
-        // Iterate through the words in reverse order
-        for (int i = str.length - 1; i > 0; i--) {
-            // Append the current word and a space to the output
-            out += str[i] + " ";
+        String[] arr = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length-1; i > 0; i--) {
+            sb.append(arr[i]).append(" ");
         }
-
-        // Append the first word to the output (without trailing space)
-        return out + str[0];
+        sb.append(arr[0]);
+        return sb.toString();
     }
 }
