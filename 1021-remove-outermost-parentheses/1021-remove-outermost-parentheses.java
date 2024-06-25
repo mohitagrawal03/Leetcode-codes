653af29f1944af671a056c973be24bf17cdd39d0
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-25 18:32:00 +0530

    Time: 4 ms (70.53%), Space: 42 MB (60.19%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,65 +1,45 @@
-// class Solution {
-    
-//     // Time complexity O(n) and works and good
-// //     public String removeOuterParentheses(String s) {
-// //         Stack<Character> st = new Stack<>();
-// //         StringBuilder ans = new StringBuilder();
-// //         for (int i = 0; i < s.length(); i++) {
-// //             if (s.charAt(i) == '(') {
-// //                 if (!st.empty()) {
-// //                     ans.append("(");
-// //                 }
-                
-// //                 st.push('(');
-// //             } else {
-// //                 st.pop();
-                
-// //                 if (!st.empty()) {
-// //                     ans.append(')');
-// //                 }
-// //             }
-// //         }
-        
-// //         return ans.toString();
-// //     }
-    
-//     // Little bit better Approach
-// //     public String removeOuterParentheses(String S) {
-        
-// //         StringBuilder ans = new StringBuilder();
-// //         int opened = 0, start = 0;
-        
-// //         for (int i = 0; i < S.length(); i++) {
-// //             if (S.charAt(i) == '(') {
-// //                 opened++;
-// //             } else {
-// //                 opened--;
-// //             }
-// //             if (opened == 0) {
-// //                 ans.append(S.substring(start+1, i));
-// //                 start = i+1;
-// //             }
-// //         }
-// //         return ans.toString();
-// //     }
-// }
-
 class Solution {
+    
+    // Time complexity O(n) and works and good
+//     public String removeOuterParentheses(String s) {
+//         Stack<Character> st = new Stack<>();
+//         StringBuilder ans = new StringBuilder();
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') {
+//                 if (!st.empty()) {
+//                     ans.append("(");
+//                 }
+                
+//                 st.push('(');
+//             } else {
+//                 st.pop();
+                
+//                 if (!st.empty()) {
+//                     ans.append(')');
+//                 }
+//             }
+//         }
+        
+//         return ans.toString();
+//     }
+    
+    // Little bit better Approach
     public String removeOuterParentheses(String S) {
         
-        StringBuilder sb = new StringBuilder();
-        int open=0, close=0, start=0;
-        for(int i=0; i<S.length(); i++) {
-            if(S.charAt(i) == '(') {
-                open++;
-            } else if(S.charAt(i) == ')') {
-                close++;
+        StringBuilder ans = new StringBuilder();
+        int opened = 0, start = 0;
+        
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                opened++;
+            } else {
+                opened--;
             }
-            if(open==close) {
-                sb.append(S.substring(start+1, i));
-                start=i+1;
+            if (opened == 0) {
+                ans.append(S.substring(start+1, i));
+                start = i+1;
             }
         }
-        return sb.toString();
+        return ans.toString();
     }
 }
