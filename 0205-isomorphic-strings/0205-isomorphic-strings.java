9bb94c3cf89d03521aa043eb48d0d52948702d43
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-29 22:17:44 +0530

    Time: 35 ms (5.17%), Space: 42.5 MB (20.89%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,14 +4,10 @@ class Solution {
         HashMap<Character, Character> hmTS = new HashMap<>();
         
         for (int i = 0; i < s.length(); i++) {
-            if (hmST.containsKey(s.charAt(i))) {
-                if (hmST.get(s.charAt(i)) != t.charAt(i)) {
+            if (hmST.containsKey(s.charAt(i)) && hmST.get(s.charAt(i)) != t.charAt(i)) {
                     return false;
-                }
-            } if (hmTS.containsKey(t.charAt(i))) {
-                if (hmTS.get(t.charAt(i)) != s.charAt(i)) {
+            } if (hmTS.containsKey(t.charAt(i)) && hmTS.get(t.charAt(i)) != s.charAt(i)) {
                     return false;
-                }
             } else {
                 hmST.put(s.charAt(i), t.charAt(i));
                 hmTS.put(t.charAt(i), s.charAt(i));
