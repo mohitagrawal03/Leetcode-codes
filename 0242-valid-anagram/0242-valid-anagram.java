9cedcc10781249877744a6d2869e5d518926751e
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-30 11:57:34 +0530

    Time: 5 ms (53.73%), Space: 42.5 MB (95.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,14 +3,13 @@ class Solution {
         if (s.length() != t.length()) return false;
         
         int[] ss = new int[256];
-        int[] tt = new int[256];
         
         for (int i = 0; i < s.length(); i++) {
             ss[s.charAt(i)]++;
-            tt[t.charAt(i)]++;
+            ss[t.charAt(i)]--;
         }
         for (int i = 0; i < 256; i++) {
-            if (ss[i] != tt[i]) return false;
+            if (ss[i] != 0) return false;
         }
         return true;
     }
