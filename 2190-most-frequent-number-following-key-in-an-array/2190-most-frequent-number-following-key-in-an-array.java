221472c79c1cdce1c750d8355af1696ed9af457b
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-30 18:29:37 +0530

    Time: 3 ms (58.33%), Space: 44.2 MB (46.94%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -2,21 +2,26 @@ class Solution {
     public int mostFrequent(int[] nums, int key) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         
+        int maxValue = 0, maxKey = 0;
         for (int i = 0; i <= nums.length-2; i++) {
             if (nums[i] == key) {
                 hm.put(nums[i+1], hm.getOrDefault(nums[i+1], 0)+1);
+                if (hm.get(nums[i+1]) > maxValue) {
+                    maxKey = nums[i+1];
+                    maxValue = hm.get(nums[i+1]);
+                }
             }
         }
         
-        int maxKey = 0;
-        int maxValue = 0;
+//         int maxKey = 0;
+//         int maxValue = 0;
         
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            if (entry.getValue() > maxValue) {
-                maxValue = entry.getValue();
-                maxKey = entry.getKey();
-            }
-        }
+//         for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+//             if (entry.getValue() > maxValue) {
+//                 maxValue = entry.getValue();
+//                 maxKey = entry.getKey();
+//             }
+//         }
         
         return maxKey;
     }
