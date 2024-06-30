1f9aa9b13252538859e97997683fba20a1ee34ca
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-30 12:00:58 +0530

    Time: 2 ms (97.08%), Space: 43.4 MB (63.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,41 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) return false;
+        
+//         int[] ss = new int[26];
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             ss[s.charAt(i) - 'a']++;
+//             ss[t.charAt(i) - 'a']--;
+//         }
+//         for (int i = 0; i < 26; i++) {
+//             if (ss[i] != 0) return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
         
-        int[] ss = new int[26];
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
         
-        for (int i = 0; i < s.length(); i++) {
-            ss[s.charAt(i) - 'a']++;
-            ss[t.charAt(i) - 'a']--;
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
         }
-        for (int i = 0; i < 26; i++) {
-            if (ss[i] != 0) return false;
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
         }
+        
         return true;
     }
 }
