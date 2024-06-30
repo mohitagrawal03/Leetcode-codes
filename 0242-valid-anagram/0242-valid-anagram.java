0cb793f8c6579254aa31b00e09a7ac8b4c8644b2
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-30 12:13:28 +0530

    Time: 1 ms (99.78%), Space: 43 MB (75.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,16 @@
+// Works and T.C. O(n) but for loop could be wriiten in better manner.
 // class Solution {
 //     public boolean isAnagram(String s, String t) {
 //         if (s.length() != t.length()) return false;
         
-//         int[] ss = new int[26];
+//         int[] count = new int[26];
         
 //         for (int i = 0; i < s.length(); i++) {
-//             ss[s.charAt(i) - 'a']++;
-//             ss[t.charAt(i) - 'a']--;
+//             count[s.charAt(i) - 'a']++;
+//             count[t.charAt(i) - 'a']--;
 //         }
 //         for (int i = 0; i < 26; i++) {
-//             if (ss[i] != 0) return false;
+//             if (count[i] != 0) return false;
 //         }
 //         return true;
 //     }
@@ -17,25 +18,19 @@
 
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        
         int[] count = new int[26];
         
-        // Count the frequency of characters in string s
-        for (char x : s.toCharArray()) {
-            count[x - 'a']++;
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
-        
-        // Decrement the frequency of characters in string t
-        for (char x : t.toCharArray()) {
-            count[x - 'a']--;
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
         }
-        
-        // Check if any character has non-zero frequency
-        for (int val : count) {
-            if (val != 0) {
-                return false;
-            }
+        for (int i : count) {
+            if(i != 0) return false;
         }
-        
         return true;
     }
 }
