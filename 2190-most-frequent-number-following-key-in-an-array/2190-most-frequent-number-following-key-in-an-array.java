3956dcfa8016936e99ed7ec7665e8fc57d751e99
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-30 18:32:29 +0530

    Time: 3 ms (58.33%), Space: 44.2 MB (46.94%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int mostFrequent(int[] nums, int key) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
+//     public int mostFrequent(int[] nums, int key) {
+//         HashMap<Integer, Integer> hm = new HashMap<>();
         
-        int maxValue = 0, maxKey = 0;
-        for (int i = 0; i <= nums.length-2; i++) {
-            if (nums[i] == key) {
-                hm.put(nums[i+1], hm.getOrDefault(nums[i+1], 0)+1);
-                if (hm.get(nums[i+1]) > maxValue) {
-                    maxKey = nums[i+1];
-                    maxValue = hm.get(nums[i+1]);
-                }
-            }
-        }
+//         int maxValue = 0, maxKey = 0;
+//         for (int i = 0; i <= nums.length-2; i++) {
+//             if (nums[i] == key) {
+//                 hm.put(nums[i+1], hm.getOrDefault(nums[i+1], 0)+1);
+//                 if (hm.get(nums[i+1]) > maxValue) {
+//                     maxKey = nums[i+1];
+//                     maxValue = hm.get(nums[i+1]);
+//                 }
+//             }
+//         }
         
 //         int maxKey = 0;
 //         int maxValue = 0;
@@ -23,6 +23,21 @@ class Solution {
 //             }
 //         }
         
-        return maxKey;
+        //return maxKey;
+    
+    public int mostFrequent(int[] nums, int key) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int mostFreq = -1;
+        for (int i = 0, n = nums.length, max = 0; i + 1 < n; ++i) {
+            if (nums[i] == key) {
+                int candidate = nums[i + 1];
+                freq.put(candidate, 1 + freq.getOrDefault(candidate, 0));
+                if (freq.get(candidate) > max) {
+                    max = freq.get(candidate);
+                    mostFreq = candidate;
+                }
+            }
+        }
+        return mostFreq;
     }
 }
