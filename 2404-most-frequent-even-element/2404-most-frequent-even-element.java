a7ef5ba77ee715bef2618676908c05e16c381aac
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-06-30 23:26:20 +0530

    Time: 20 ms (47.29%), Space: 44.9 MB (47.29%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,17 +1,30 @@
 class Solution {
+    // public int mostFrequentEven(int[] nums) {
+    //     HashMap<Integer, Integer> hm =  new HashMap<>();
+    //     Integer ans = -1;
+    //     Integer max = Integer.MIN_VALUE;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (nums[i] % 2 == 0) {
+    //             hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+    //             if (hm.get(nums[i]) > max || (hm.get(nums[i]) == max && nums[i] < ans)) {
+    //                 ans = nums[i];
+    //                 max = hm.get(nums[i]);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
     public int mostFrequentEven(int[] nums) {
         HashMap<Integer, Integer> hm =  new HashMap<>();
         Integer ans = -1;
         Integer max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
-                if (hm.get(nums[i]) > max) {
-                    ans = nums[i];
-                    max = hm.get(nums[i]);
-                } else if (hm.get(nums[i]) == max && nums[i] < ans) {
-                    ans = nums[i];
-                    max = hm.get(nums[i]);
+        for (int i : nums) {
+            if (i % 2 == 0) {
+                hm.put(i, hm.getOrDefault(i, 0)+1);
+                if (hm.get(i) > max || (hm.get(i) == max && i < ans)) {
+                    ans = i;
+                    max = hm.get(i);
                 }
             }
         }
