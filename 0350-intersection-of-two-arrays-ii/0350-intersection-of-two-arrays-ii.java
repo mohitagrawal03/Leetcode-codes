0445ef0676ccd58897c9e8ec30de37303a4d9f47
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-03 15:53:52 +0530

    Time: 3 ms (91.02%), Space: 43 MB (58.43%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -23,24 +23,47 @@ class Solution {
 //         return ans;
         
         // Follow Up: if arrays are sorted.
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         List<Integer> list = new ArrayList<>();
+//         int i = 0, j = 0;
+//         while (i < nums1.length && j < nums2.length) {
+//             if (nums1[i] == nums2[j]) {
+//                 list.add(nums1[i]);
+//                 i++;
+//                 j++;
+//             } else if (nums1[i] < nums2[j]) {
+//                 i++;
+//             } else {
+//                 j++;
+//             }
+//         }
+        
+//         int[] ans = new int[list.size()];
+//         i = 0;
+//         for(int num : list) {
+//             ans[i++] = num;
+//         }
+        
+//         return ans;
+        
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        
+        for (int num : nums1) {
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+        
         List<Integer> list = new ArrayList<>();
-        int i = 0, j = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] == nums2[j]) {
-                list.add(nums1[i]);
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
+        
+        for (int num : nums2) {
+            if (freq.containsKey(num) && freq.get(num) > 0) {
+                list.add(num);
+                freq.put(num, freq.get(num)-1);
             }
         }
         
         int[] ans = new int[list.size()];
-        i = 0;
+        int i = 0;
         for(int num : list) {
             ans[i++] = num;
         }
