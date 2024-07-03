8b58c7b98729355e492bc1f444653685167de473
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-03 13:27:48 +0530

    Time: 3 ms (91.02%), Space: 42.7 MB (92.27%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,19 +1,46 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    list.add(nums2[j]);
-                    nums2[j] = -1;
-                    break;
-                }
+        // Good Approach and Consider it.
+//         List<Integer> list = new ArrayList<>();
+        
+//         for (int i = 0; i < nums1.length; i++) {
+//             for (int j = 0; j < nums2.length; j++) {
+//                 if (nums2[j] == nums1[i]) {
+//                     list.add(nums2[j]);
+//                     nums2[j] = -1;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         int[] ans = new int[list.size()];
+//         int i = 0;
+//         for(int num : list) {
+//             ans[i++] = num;
+//         }
+        
+//         return ans;
+        
+        // Follow Up: if arrays are sorted.
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         
         int[] ans = new int[list.size()];
-        int i = 0;
+        i = 0;
         for(int num : list) {
             ans[i++] = num;
         }
