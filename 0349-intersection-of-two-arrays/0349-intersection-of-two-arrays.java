b3a5e855d58cc468ed6bcd2070e7bfd67182beca
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-03 12:38:55 +0530

    Time: 2 ms (96.21%), Space: 43.4 MB (17.87%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,16 +1,16 @@
+// Not so good T.C., Theoritically I think O(n) hi hai but actually (1000n) something
 // class Solution {
 //     public int[] intersection(int[] nums1, int[] nums2) {
-        
 //         List<Integer> ans = new ArrayList<>();
-        
+
 //         for (int i = 0; i <= 1000; i++) {
 //             boolean bol1 = false, bol2 = false;
-//             for (int j = 0; i < nums1.length; i++ ) {
+//             for (int j = 0; j < nums1.length; j++) { 
 //                 if (nums1[j] == i) {
 //                     bol1 = true;
 //                 }
 //             }
-//             for (int j = 0; i < nums2.length; i++ ) {
+//             for (int j = 0; j < nums2.length; j++) { 
 //                 if (nums2[j] == i) {
 //                     bol2 = true;
 //                 }
@@ -29,28 +29,25 @@
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> ans = new ArrayList<>();
-
-        for (int i = 0; i <= 1000; i++) {
-            boolean bol1 = false, bol2 = false;
-            for (int j = 0; j < nums1.length; j++) {  // Use 'j' and correct condition
-                if (nums1[j] == i) {
-                    bol1 = true;
-                }
-            }
-            for (int j = 0; j < nums2.length; j++) {  // Use 'j' and correct condition
-                if (nums2[j] == i) {
-                    bol2 = true;
-                }
-            }
-            if (bol1 && bol2) {
-                ans.add(i);
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> intersect = new HashSet<>();
+        
+        for (int i = 0; i < nums1.length; i++) {
+           set.add(nums1[i]); 
+        }
+        
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
             }
         }
-        int[] array = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            array[i] = ans.get(i);
+        
+        int[] ans = new int[intersect.size()];
+        int i = 0;
+        for (int num : intersect) {
+            ans[i++] = num;
         }
-        return array;
+        
+        return ans;
     }
 }
