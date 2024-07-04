7605c812881851eff9cecd46d902ce25e891fecc
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-04 12:11:34 +0530

    Time: 2 ms (19.86%), Space: 45.4 MB (71.21%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        //List<List<Integer>> idx = new ArrayList<>();
+        HashSet<Integer> row = new HashSet<>();
+        HashSet<Integer> col = new HashSet<>();
+        
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if(matrix[r][c] == 0) {
+                    // List<Integer> temp = new ArrayList<>();
+                    // temp.add(r);
+                    // temp.add(c);
+                    // idx.add(temp);
+                    row.add(r);
+                    col.add(c);
+                }
+            }
+        }
+        
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if(row.contains(r) || col.contains(c)) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+}
