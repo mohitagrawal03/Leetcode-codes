b9f115277064bdc8a9beabfb6a3c81521020b94a
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-05 17:24:09 +0530

    Time: 7 ms (100.00%), Space: 44 MB (39.66%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,10 +1,28 @@
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+//         if (n < 1) return false;
+//         if (n == 1 || n == 3) return true;
+        
+//         if (n%9 == 0) return isPowerOfThree(n/3);
+        
+//         return false;
+//     }
+// }
+
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+//         if (n < 1) return false;
+//         if (n == 1) return true;
+        
+//         if (n%3 == 0) return isPowerOfThree(n/3);
+        
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n < 1) return false;
-        if (n == 1 || n == 3) return true;
-        
-        if (n%9 == 0) return isPowerOfThree(n/3);
-        
-        return false;
+    // 1162261467 is 3^19,  3^20 is bigger than int (works for prime numbers)  
+    return ( n>0 &&  1162261467%n==0);
     }
 }
