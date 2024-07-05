fe00cb50d311f732bfce6d9e590a055f79f062d8
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-05 23:49:33 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (63.40%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -21,8 +21,18 @@
 //     }
 // }
 
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         return (n > 0) && (n & n-1) == 0 && Integer.toBinaryString(n).length() % 2 != 0;
+//     }
+// }
+
 class Solution {
-    public boolean isPowerOfFour(int n) {
-        return (n > 0) && (n & n-1) == 0 && Integer.toBinaryString(n).length() % 2 != 0;
-    }
+     public boolean isPowerOfFour(int n) {
+         if (n < 1) return false;
+         
+         double logBase4 = Math.log(n)/Math.log(4);
+         
+         return logBase4 == (int)logBase4;
+     }
 }
