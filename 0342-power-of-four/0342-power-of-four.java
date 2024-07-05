8c8b1b03795722628b4502c96d8fb553687a1e47
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-06 00:07:54 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (23.90%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -36,3 +36,9 @@ class Solution {
          return logBase4 == (int)logBase4;
      }
 }
+
+// public boolean isPowerOfFour(int n) {
+//        if(n<=0) return false;
+//        if((n&(n-1))!=0) return false;
+//         return (n-1)%3==0;
+//     }
