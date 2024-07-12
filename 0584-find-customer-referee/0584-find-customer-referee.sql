f00f21217af2a5ab867d216cd7925ba4f3e0d2c0
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-12 11:32:27 +0530

    Time: 445 ms (71.77%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-SELECT name FROM Customer WHERE COALESCE(referee_id,0) <> 2;
+# SELECT name FROM Customer WHERE COALESCE(referee_id,0) <> 2;
+SELECT name FROM Customer WHERE IFNULL(referee_id,0) <> 2;
