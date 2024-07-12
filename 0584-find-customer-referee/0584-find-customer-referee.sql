5d469f31875c8b37ff1938300c06e1d5a13d70f1
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-12 11:39:58 +0530

    Time: 625 ms (43.18%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
 # SELECT name FROM Customer WHERE COALESCE(referee_id,0) <> 2;
 # SELECT name FROM Customer WHERE IFNULL(referee_id,0) <> 2;
-SELECT name FROM Customer WHERE referee_id != 2 OR referee_id is NULL;
+# SELECT name FROM Customer WHERE referee_id != 2 OR referee_id is NULL;
+SELECT name 
+FROM Customer 
+WHERE id NOT IN (SELECT id
+                 FROM Customer
+                 WHERE referee_id = 2);
