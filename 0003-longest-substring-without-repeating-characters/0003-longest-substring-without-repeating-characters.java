a6e9f812ee223b567ea7b0ad565346898b5482d0
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-07-17 16:16:16 +0530

    Time: 6 ms (68.20%), Space: 44.7 MB (36.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -23,23 +23,40 @@
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() <= 1) return s.length();
+        // BRUTE FORCE
+//         if (s.length() <= 1) return s.length();
         
-        HashSet<Character> letters = new HashSet<>();
-        int i = 0, j = 0;  // Initialize j to 0
-        int max = 0;       // Initialize max to 0
+//         HashSet<Character> letters = new HashSet<>();
+//         int i = 0, j = 0;  // Initialize j to 0
+//         int max = 0;       // Initialize max to 0
         
-        while (i < s.length()) {
-            if (!letters.contains(s.charAt(i))) {
-                letters.add(s.charAt(i));
-                max = Math.max(max, i - j + 1); // Calculate the max length
-                i++;
+//         while (i < s.length()) {
+//             if (!letters.contains(s.charAt(i))) {
+//                 letters.add(s.charAt(i));
+//                 max = Math.max(max, i - j + 1); // Calculate the max length
+//                 i++;
+//             } else {
+//                 letters.remove(s.charAt(j));
+//                 j++;
+//             }
+//         }
+        
+//         return max;
+        
+        // 2 Pointer Approach
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int l = 0, r = 0, maxlen = 0;
+        
+        while (r < s.length()) {
+            if (!hm.containsKey(s.charAt(r)) || hm.get(s.charAt(r)) < l) {
+                maxlen = Math.max(maxlen, r-l+1);
+                hm.put(s.charAt(r), r);
+                r++;
             } else {
-                letters.remove(s.charAt(j));
-                j++;
+                    l = hm.get(s.charAt(r))+1;
             }
         }
         
-        return max;
+        return maxlen;
     }
 }
