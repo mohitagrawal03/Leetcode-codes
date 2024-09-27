badddcde814de4a9f410c050d6f4ccc2dbca7347
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-09-27 21:32:03 +0530

    Time: 627 ms (33.47%), Space: 0B (100.00%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -6,3 +6,6 @@
 SELECT tweet_id
 FROM Tweets
 WHERE CHAR_LENGTH(content) > 15
+
+# LENGTH() returns the length of the string measured in bytes.
+# CHAR_LENGTH() returns the length of the string measured in characters.
